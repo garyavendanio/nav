5e910c61d36e17ce2d0fa9e4c1cfeb830488f007
garyavendanio <avenros1@gmail.com>
2019-04-05 14:04:24 -0500

    navbar jscss

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const navSlide = () => {
             if(link.style.animation) {
                 link.style.animation = '';
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+                link.style.animation = `navLinkFade .5s ease forwards ${index / 7 + 1}s`;
             }
         });
         //Burger Animation
